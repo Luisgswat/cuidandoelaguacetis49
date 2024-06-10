ebd0c1e2ef715a05ff5e8530cfbefe5a782c7d0a
Luisgswat <lggtz159@gmail.com>
2024-06-10 02:56:12 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+﻿var correctAnswer = "500 litros";
+  
+function checkAnswer(answer) {
+  if (answer === true) {
+    document.getElementById("result").innerHTML = "¡Correcto! Se necesitan aproximadamente 500 litros de agua para producir una hamburguesa.";
+  } else {
+    document.getElementById("result").innerHTML = "Incorrecto. La respuesta correcta es 500 litros.";
+  }
+}
